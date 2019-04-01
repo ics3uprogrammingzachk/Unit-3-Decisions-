85dcd32acfc14e807a80f95d15e9844beff16001
ZacharyKr <32547158+ZacharyKr@users.noreply.github.com>
2019-04-01 12:10:00 -0400

    added

---
+++ PerfectSquaresWhileLoopZach/PerfectSquaresWhileLoopZach/PerfectSquaresWhileLoopForm.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace PerfectSquaresWhileLoopZach
+{
+    partial class PerfectSquaresWhileLoopForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.nudEndValue = new System.Windows.Forms.NumericUpDown();
+            this.lstPerfectSquares = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndValue)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.BackColor = System.Drawing.Color.White;
+            this.btnCalculate.ForeColor = System.Drawing.Color.Black;
+            this.btnCalculate.Location = new System.Drawing.Point(298, 12);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(220, 78);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = false;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // nudEndValue
+            // 
+            this.nudEndValue.BackColor = System.Drawing.Color.White;
+            this.nudEndValue.Location = new System.Drawing.Point(282, 106);
+            this.nudEndValue.Name = "nudEndValue";
+            this.nudEndValue.Size = new System.Drawing.Size(254, 26);
+            this.nudEndValue.TabIndex = 1;
+            // 
+            // lstPerfectSquares
+            // 
+            this.lstPerfectSquares.Font = new System.Drawing.Font("Times New Roman", 16F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstPerfectSquares.FormattingEnabled = true;
+            this.lstPerfectSquares.ItemHeight = 36;
+            this.lstPerfectSquares.Location = new System.Drawing.Point(193, 153);
+            this.lstPerfectSquares.Name = "lstPerfectSquares";
+            this.lstPerfectSquares.Size = new System.Drawing.Size(445, 256);
+            this.lstPerfectSquares.TabIndex = 2;
+            // 
+            // PerfectSquaresWhileLoopForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstPerfectSquares);
+            this.Controls.Add(this.nudEndValue);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "PerfectSquaresWhileLoopForm";
+            this.Text = "Perfect Squares";
+            ((System.ComponentModel.ISupportInitialize)(this.nudEndValue)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.NumericUpDown nudEndValue;
+        private System.Windows.Forms.ListBox lstPerfectSquares;
+    }
+}
+
---
+++ PerfectSquaresWhileLoopZach/PerfectSquaresWhileLoopZach/PerfectSquaresWhileLoopForm.cs
@@ -0,0 +1,65 @@
+﻿/*
+ * Created by: Zachary Kranabetter
+ * Created on: March 27 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #22 - Perfect Squares with While Loop
+ * This program lets the user enter a number with a numeric up-down box 
+ * and then the program calculates each perfect square up to that number
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquaresWhileLoopZach
+{
+    public partial class PerfectSquaresWhileLoopForm : Form
+    {
+        public PerfectSquaresWhileLoopForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // declare local variables
+            int endingValue;
+            int value = 1;
+            Double squareRootAsDouble;
+            int squareRootAsInteger;
+
+            // clear the listbox
+            lstPerfectSquares.Items.Clear();
+
+            // get the user's end value selection from the numeric updown box
+            endingValue = Convert.ToInt32(this.nudEndValue.Value);
+
+            while (value <= endingValue)
+            {
+                // take the square root of the value
+                squareRootAsDouble = Math.Sqrt(value);
+
+                // convert the double to an integer
+                squareRootAsInteger = Convert.ToInt32(squareRootAsDouble);
+
+                // check if it's a perfect square
+                if (squareRootAsInteger == squareRootAsDouble)
+                {
+                    // tell the user that this number is a perfect square
+                    this.lstPerfectSquares.Items.Add(value + " is a perfect square");
+                }
+
+                // refresh the screen
+                this.Refresh();
+
+                // increment the value
+                value = value + 1;
+            }
+        }
+    }
+}
